e28af1cb2c555f3d37f7ad971ee7c782d42f5ea0
Stephine <stephine.sinoy@cit.edu>
2023-03-13 16:06:53 +0800

    Update evaluator

--- interpreter/Functions/Evaluator.cs
+++ interpreter/Functions/Evaluator.cs
@@ -3,42 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using interpreter.Grammar;
 
 namespace interpreter
 {
     public class Evaluator
     {
-        public bool CheckDataType(string dataType, object? value)
+        private static bool EvaluateDataType(string dataType, object? value)
         {
             if (value == null)
             {
                 return true; // Null can be assigned to any data type
             }
 
-            switch (dataType)
+            return dataType switch
             {
-                case "INT":
-                    return value is int;
-                case "FLOAT":
-                    return value is float;
-                case "BOOL":
-                    return value is bool;
-                case "STRING":
-                    return value is string;
-                case "CHAR":
-                    return value is char;
-                default:
-                    return false; // Invalid data type
-            }
+                "INT" => value is int,
+                "FLOAT" => value is float,
+                "BOOL" => value is bool,
+                "STRING" => value is string,
+                "CHAR" => value is char,
+                _ => false,// Invalid data type
+            };
         }
 
-        public void CheckDeclaration(string dataType, string[] varNames, object? value)
+        public static void EvaluateDeclaration(string dataType, string[] varNames, object? value)
         {
             bool isValid = true;
 
             foreach (var name in varNames)
             {
-                if (!CheckDataType(dataType, value))
+                if (!EvaluateDataType(dataType, value))
                 {
                     Console.WriteLine($"Invalid value assigned to variable {name}");
                     isValid = false;
@@ -48,6 +43,81 @@ namespace interpreter
             {
                 Environment.Exit(1);
             }
+        }
+
+        public static bool EvaluateDelimiter(CodeParser.LineBlockContext context)
+        {
+            string beginDelimiter = "BEGIN CODE";
+            string endDelimiter = "END CODE";
+            string? beginCode = context.BEGIN_CODE()?.GetText();
+            string? endCode = context.END_CODE()?.GetText();
+
+            //Both delimiters are present in the code
+            if (beginCode != null && endCode != null && beginCode.Equals(beginDelimiter) && endCode.Equals(endDelimiter))
+            {
+                // Visit the declarations and lines only if the delimiters are at the beginning and end of the program
+                if (context.declaration().Length == 0 && context.line().Length == 0)
+                {
+                    Console.WriteLine("CODE not recognized \n" +
+                                       "Delimiters must be in the right position");
+                    Environment.Exit(1);
+                }
+
+                // Declaration contains BEGIN CODE
+                if (context.declaration().Length > 0 && context.declaration().Any(d => d.GetText().Contains(beginDelimiter)))
+                {
+                    Console.WriteLine($"{beginDelimiter} must only be at the beginning of the program");
+                    Environment.Exit(1);
+                }
+
+                // Line contains BEGIN CODE
+                if (context.line().Length > 0 && context.line().Any(l => l.GetText().Contains(beginDelimiter)))
+                {
+                    Console.WriteLine($"{beginDelimiter} must be only at the beginning of the program");
+                    Environment.Exit(1);
+                }
+
+                //_isBeginCodeVisited = true;
+
+                // NOT WORKING
+                // Declaration contains END CODE
+                if (context.declaration().Length > 0 && context.declaration().Any(d => d.GetText().Contains(endDelimiter) && d.GetText().IndexOf(endDelimiter) > 0))
+                {
+                    Console.WriteLine($"{endDelimiter} must be only at the end of the program");
+                    Environment.Exit(1);
+                }
+
+                // Line contains END CODE
+                if (context.line().Length > 0 && context.line().Any(l => l.GetText().Contains(endDelimiter) && l.GetText().IndexOf(endDelimiter) < l.GetText().Length - endDelimiter.Length))
+                {
+                    Console.WriteLine($"{endDelimiter} must be only at the end of the program");
+                    Environment.Exit(1);
+                }
+               
+                return true;
+            }
+            else if ((beginCode == null || !beginCode.Equals(beginDelimiter)) && (endCode != null && endCode.Equals(endDelimiter)))
+            {
+                // Only the end delimiter is present
+                Console.WriteLine("Missing BEGIN CODE delimiter");
+                Environment.Exit(1);
+            }
+
+            else if ((beginCode != null && beginCode.Equals(beginDelimiter)) && (endCode == null || !endCode.Equals(endDelimiter)))
+            {
+                // Only the begin delimiter is present
+                Console.WriteLine("Missing END CODE delimiter");
+                Environment.Exit(1);
+            }
+
+            else
+            {
+                // Neither delimiter is present
+                Console.WriteLine("Missing delimiters");
+                Environment.Exit(1);
+            }
+
+            return false;
         }
     }
 }
