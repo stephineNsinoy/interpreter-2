38905f3263243f0770639fced412bc762660629b
Stephine <stephine.sinoy@cit.edu>
2023-03-20 16:01:20 +0800

    Implement UnaryValue

--- interpreter/Functions/Operator.cs
+++ interpreter/Functions/Operator.cs
@@ -294,6 +294,43 @@ namespace interpreter.Functions
         }
 
 
+       /******************************
+              Unary expressions
+       *******************************/
+       public static object? UnaryValue(string symbol, object? expressionValue)
+        {
+            if (expressionValue is int intValue)
+            {
+                if (symbol == "+")
+                {
+                    return intValue;
+                }
+                else if (symbol == "-")
+                {
+                    return -intValue;
+                }
+            }
+            else if (expressionValue is float floatValue)
+            {
+                if (symbol == "+")
+                {
+                    return floatValue;
+                }
+                else if (symbol == "-")
+                {
+                    return -floatValue;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Unary operator {symbol} cannot be applied to non-numeric value.");
+                Environment.Exit(1);
+            }
+
+            return null;
+        }
+
+
         /******************************
             WHILE loop expressions
         *******************************/
