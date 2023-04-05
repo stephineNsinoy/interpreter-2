fb6d8fb9977c7d314ceec4cdad17f1a2da88a7cd
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-04-05 19:47:21 +0800

    Modify visitor to check for float values

--- interpreter/CodeVisitor.cs
+++ interpreter/CodeVisitor.cs
@@ -386,15 +386,30 @@ namespace interpreter
         }
 
         /// <summary>
-        /// Visits the increment expression, calls the Increment function and evaluates it.
+        /// Visits the increment expression, evaluates whether it is a float or an int
+        /// then returns the incremented value, returns an error otherwise.
         /// </summary>
         public override object? VisitIncrementExpression([NotNull] CodeParser.IncrementExpressionContext context)
         {
-            var identifier = int.TryParse(Visit(context.expression())?.ToString(), out int value);
+            var isInt = int.TryParse(Visit(context.expression())?.ToString(), out int intValue);
 
+            var isFloat = float.TryParse(Visit(context.expression())?.ToString(), out float floatValue);
+                
             var symbol = context.INCREMENT().GetText();
 
-            return FunctionsOp.IncrementValue(symbol, value, identifier);
+            SemanticErrorEvaluator.EvaluateBoolValues(isInt, isFloat);
+
+            if (isFloat)
+            {
+                return symbol.Equals("++") ? floatValue + 1 : floatValue - 1;
+            }
+
+            if (isInt)
+            {
+                return symbol.Equals("++") ? intValue + 1 : intValue - 1;
+            }
+            
+            return null;
         }
     }
 }
